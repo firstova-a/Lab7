2e91d3df6510594ddb03e88544a75b5e54cd370f
firstova-a <firstova-a@yandex.ru>
2021-05-08 15:39:41 +0300

    Добавьте файлы проекта.

---
+++ Laboratory7/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory7
+{
+   
+
+   
+    public class Address
+    {
+        public string amenity { get; set; }
+        public string road { get; set; }
+        public string village { get; set; }
+        public string state_district { get; set; }
+        public string state { get; set; }
+        public string postcode { get; set; }
+        public string country { get; set; }
+        public string country_code { get; set; }
+    }
+
+
+    public class SearchJson
+    {
+        //public int place_id { get; set; }
+        //public string licence { get; set; }
+        //public string osm_type { get; set; }
+       // public int osm_id { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+       // public int place_rank { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+        //public int importance { get; set; }
+        public string addresstype { get; set; }
+        public string name { get; set; }
+        public string display_name { get; set; }
+        public Address address { get; set; }
+        public List<string> boundingbox { get; set; }
+
+    }
+}
+
---
+++ Laboratory7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Laboratory7
+{
+    class Program
+    {
+        
+
+        static async Task Main(string[] args)
+        {
+            bool run = true;
+            while (run)
+            {   
+                Console.WriteLine("Do you want to know the address by coordinates? Enter 'n' if no, any other character if yes");
+                string marker = Console.ReadLine();
+                if (marker == "n")
+                {
+                    run = false;                    
+                }
+                else
+                {
+                    Console.WriteLine("Enter latitude (decimal separator '.')");
+                    string lat = Console.ReadLine();
+                    Console.WriteLine("Enter longitude (decimal separator '.')");
+                    string lon = Console.ReadLine();
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+                        HttpResponseMessage searchRespons = await client.GetAsync($"https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}");
+
+                        if (searchRespons.IsSuccessStatusCode)
+                        {
+                            SearchJson answer = await searchRespons.Content.ReadFromJsonAsync<SearchJson>();
+                            if (answer.display_name != null)
+                            {
+                                Console.WriteLine("Full addres:" + answer.display_name);
+                            }
+                            else
+                            {
+                                Console.WriteLine("This point on the map has no address");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed request");
+                        }
+                    }
+                    
+                }               
+            }
+        }
+    }
+}
